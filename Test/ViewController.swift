9e6555c6d460f6cda8e8d5ff7503d53a457ef4c6
VietLV <vietlv@solarapp.asia>
2020-11-15 10:23:55 +0700

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Viet Le Van on 11/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
